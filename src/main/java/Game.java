6b0316ce60a92abab300fa2eb82e018da814d730
BaekGahyeon <goginala@g.skku.edu>
2026-04-03 13:51:58 +0900

    [feature] 59p

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,21 +1,28 @@
 public class Game {
-    public void guess(String guessNumber) {
+    public String question;
+
+    public GuessResult guess(String guessNumber) {
         assertIllegalArgument(guessNumber);
+        if (guessNumber.equals(question)) {
+            return new GuessResult(true, 3, 0);
+        } else {
+            return null;
+        }
     }
 
     private static void assertIllegalArgument(String guessNumber) {
-        if (guessNumber == null){
+        if (guessNumber == null) {
             throw new IllegalArgumentException();
         }
-        if(guessNumber.length() != 3){
+        if (guessNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
-        for (char number: guessNumber.toCharArray()) {
-            if(number < '0' || number > '9'){
+        for (char number : guessNumber.toCharArray()) {
+            if (number < '0' || number > '9') {
                 throw new IllegalArgumentException();
             }
         }
-        if(isDuplicatedNumber(guessNumber)){
+        if (isDuplicatedNumber(guessNumber)) {
             throw new IllegalArgumentException();
         }
     }
